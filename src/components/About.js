0043c2de74630d4f817c3324476124465efd886a
MichalIrzylowski <michal.irzylowski@gmail.com>
2018-07-19 15:52:48 +0200

    Updates about

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ const About = () => (
     <p className='About-Header'><b>Cześć!</b></p>
       <p>Mam na imię Michał i chcę zostać programistą webowym. Z wykształcenia jestem chemikiem i obecnie pracuję jako chemik analityk w jednej z najszybciej rozwijających się firm biotechnologicznych w Polsce.</p>
       <p>Około półtora roku temu postanowiłem rozwinąć swoje zainteresowanie, którym jest programowanie. Naukę zacząłem od C++. Szybko okazało się, że bardziej wciąga mnie programowanie webowe. W końcu zacząłem tworzyć proste strony w HTML i CSS.</p>
-      <p>Po opanowaniu podstaw tych języków naturalnym krokiem była nauka JavaScriptu. Po poznaniu podstaw JS'a doszedłem do wniosku, że właśnie tym chcę zajmować się w życiu.</p>
+      <p>Po opanowaniu podstaw tych języków naturalnym krokiem była nauka JavaScriptu. Po zapoznaniu się z JS'em doszedłem do wniosku, że właśnie tym chcę zajmować się w życiu.</p>
       <p>Swobodnie czuję się w React, Redux, React-Router, Node.js, ES6, CSS3 i HTML5. Obecnie szukam pracy jako <strong>Front-end developer</strong> (z nastawieniem na React) albo <strong>Full-Stack developer</strong>.</p>
       <p>W dalszych planach mam zamiar poznać Webpack oraz inny back-endowy język programowania (np.: Python albo Java).</p>
 
