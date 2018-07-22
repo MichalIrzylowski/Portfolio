4d68c42c8a3b712280dc1a8e194126c26b27fa26
MichalIrzylowski <michal.irzylowski@gmail.com>
2018-07-22 11:20:15 +0200

    Updates contrast colors and general styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import {ProfileImage} from './Image';
 const FaceCard = () => (
   <div className='Card'>
     <ProfileImage />
-    <p className='Card-Item'><i className='far fa-envelope' ></i> <strong>email:</strong> michal.irzylowski@gmail.com</p>
+    <p className='Card-Item'><i className='far fa-envelope' ></i> <strong>email:</strong> <a href="mailto:michal.irzylowski@gmail.com">michal.irzylowski@gmail.com</a></p>
     <p className='Card-Item'><i className='fas fa-phone' ></i> <strong>tel.:</strong> 794 731 291</p>
     <p className='Card-Item'><i className='fab fa-github' ></i> <strong>GitHub:</strong> <a target="_blank" rel="noopener noreferrer" href='https://github.com/MichalIrzylowski'>https://github.com/MichalIrzylowski</a></p>
   </div>
